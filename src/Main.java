644b02a30769fb1ec779c08c146cf07a84baad25
Wzooh <sfzp74@gmail.com>
2022-11-12 19:58:41 +0500

    HomeW 4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
     }
 
     public static void task1 () {
@@ -28,5 +30,28 @@ public class Main {
     }
     public static void task3 () {
         System.out.println("Задача 3");
+        int speed = 60;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + " то, придется заплатить штраф.");
+        } else {
+            System.out.println("Если скорость " + speed + " то, можно ездить спокойно.");
+        }
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        int age = 19;
+        if (age >= 2 && age < 6) {
+            System.out.println("Если возраст человека равен " + age + "то ему нужно ходить  в садик");
+        }
+        if (age >= 7 && age < 18) {
+            System.out.println("Если возраст человека равен " + age + "то ему нужно ходить в школу");
+        }
+        if (age >= 18 && age < 24) {
+            System.out.println("Если возраст человека равен " + age + "то ему нужно ходить в универ");
+        }
+        if (age > 24) {
+            System.out.println("Если возраст человека равен " + age + "то ему нужно ходить на работу");
+        }
+    }
 
 }
